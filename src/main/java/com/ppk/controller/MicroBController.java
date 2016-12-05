93191ffd12d3ac611093a9a3b526d5ec22339218
Prasad Kulkarni <ppk6585@gmail.com>
2016-12-06 01:24:39 +0530

    drop wizard metrics changes

--- src/main/java/com/ppk/controller/MicroBController.java
+++ src/main/java/com/ppk/controller/MicroBController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ppk.service.MicroBServiceImpl;
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+import com.ppk.service.MicroBService;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
@@ -16,14 +18,18 @@ import com.wordnik.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping
 public class MicroBController {
+	
+	private static final String		MS_BASE_METRIC_URL	= "/ms/v2/content";
 
-	private final MicroBServiceImpl microAService;
+	private final MicroBService microAService;
 
 	@Autowired
-	public MicroBController(MicroBServiceImpl microAService) {
+	public MicroBController(MicroBService microAService) {
 		this.microAService = microAService;
 	}
 
+	@Timed(name = MS_BASE_METRIC_URL + "/.GET", absolute = true)
+	@ExceptionMetered(name = MS_BASE_METRIC_URL + "/.GET." + ExceptionMetered.DEFAULT_NAME_SUFFIX, absolute = true)
 	@ApiOperation(value = "Get content  ", notes = "Get content ")
 	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Map<String, String> welcomeMessage() {
--- src/main/java/com/ppk/service/MicroBServiceImpl.java
+++ src/main/java/com/ppk/service/MicroBServiceImpl.java
@@ -5,9 +5,14 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
+
 @Service
 public class MicroBServiceImpl implements MicroBService {
 
+	@Timed
+	@ExceptionMetered
 	@Override
 	public Map<String, String> getContent() {
 		Map<String, String> content = new HashMap<>();
